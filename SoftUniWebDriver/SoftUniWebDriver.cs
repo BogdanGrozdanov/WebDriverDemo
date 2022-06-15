0764a9947a731a12ff256449bda43ed97ab51217
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2022-06-15 13:01:24 +0300

    change

---
+++ SoftUniWebDriver/SoftUniWebDriver.cs
@@ -0,0 +1,10 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+var driver = new ChromeDriver();
+driver.Url = "https://softuni.bg";
+driver.Manage().Window.Maximize();
+
+var aboutUs = driver.FindElement(By.CssSelector("li:nth-of-type(1) > .nav-link > .cell"));
+aboutUs.Click();
+System.Console.WriteLine(driver.Title);
