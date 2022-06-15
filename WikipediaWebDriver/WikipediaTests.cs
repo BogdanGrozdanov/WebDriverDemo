8b0dbe880d9387e21e8c8ddfb606f056bac294a7
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2022-06-15 12:53:45 +0300

    write some code

---
+++ WikipediaWebDriver/WikipediaTests.cs
@@ -0,0 +1,13 @@
+﻿
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+var driver = new ChromeDriver();
+driver.Url = "https://wikipedia.org";
+driver.Manage().Window.Maximize();
+
+var searchField = driver.FindElement(By.CssSelector("input#searchInput"));
+searchField.Click();
+searchField.SendKeys("QA" + Keys.Enter);
+//searchField.SendKeys(Keys.Enter);
+driver.Quit();
