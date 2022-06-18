d3525842f9785ea175ddc54e4f3104680293b1de
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2022-06-18 19:06:17 +0300

    Add coments

--- WikipediaWebDriver/WikipediaTests.cs
+++ WikipediaWebDriver/WikipediaTests.cs
@@ -1,19 +1,28 @@
 ﻿
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-
+//Create driver
 var driver = new ChromeDriver();
+//driver URL
 driver.Url = "https://wikipedia.org";
+//Maximize screen
 driver.Manage().Window.Maximize();
 
-
+// Print title page
 Console.WriteLine("Current Title :  " + driver.Title);
+//Find element
 var searchField = driver.FindElement(By.CssSelector("input#searchInput"));
+//click on element
 searchField.Click();
+//input on element
 searchField.SendKeys("QA" + Keys.Enter);
+//Print title element
 Console.WriteLine("Title now :" +driver.Title);
+//search element
 searchField = driver.FindElement(By.CssSelector("input#searchInput"));
+//enter input
 searchField.SendKeys("Bulgaria" + Keys.Enter);
+//Print title page
 Console.WriteLine("Title:" + driver.Title);
-//searchField.SendKeys(Keys.Enter);
+//close driver(browser)
 driver.Quit();
