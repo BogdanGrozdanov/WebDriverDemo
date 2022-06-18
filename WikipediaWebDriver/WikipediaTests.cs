9af4696b74f94658482100ed01bc4461c594855f
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2022-06-18 13:57:16 +0300

    add serches

--- WikipediaWebDriver/WikipediaTests.cs
+++ WikipediaWebDriver/WikipediaTests.cs
@@ -6,8 +6,14 @@ var driver = new ChromeDriver();
 driver.Url = "https://wikipedia.org";
 driver.Manage().Window.Maximize();
 
+
+Console.WriteLine("Current Title :  " + driver.Title);
 var searchField = driver.FindElement(By.CssSelector("input#searchInput"));
 searchField.Click();
 searchField.SendKeys("QA" + Keys.Enter);
+Console.WriteLine("Title now :" +driver.Title);
+searchField = driver.FindElement(By.CssSelector("input#searchInput"));
+searchField.SendKeys("Bulgaria" + Keys.Enter);
+Console.WriteLine("Title:" + driver.Title);
 //searchField.SendKeys(Keys.Enter);
 driver.Quit();
