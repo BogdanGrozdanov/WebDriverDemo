45be8dcbda0724691338d916dad76711e5fc6b0d
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2022-06-18 20:10:58 +0300

    Add tests for SoftUni

--- SoftUniWebDriver/SoftUniTests.cs
+++ SoftUniWebDriver/SoftUniTests.cs
@@ -1,15 +1,37 @@
 ﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
 public class SoftUniTests
 {
+    private ChromeDriver softUni_Driver;
+
     [SetUp]
     public void SetUp()
-    { 
-    
+    {
+        this.softUni_Driver = new ChromeDriver();
+        softUni_Driver.Url = "https://softuni.bg";
+        softUni_Driver.Manage().Window.Maximize();
+    }
+
+    [TearDown]
+    public void Close() => softUni_Driver?.Close();
+
+    [Test]
+    public void Assert_Main_Page_Title()
+    {
+        string exeptedTitle = "Обучение по програмиране - Софтуерен университет";
+        Assert.That(softUni_Driver.Title, Is.EqualTo(exeptedTitle));
     }
 
     [Test]
-    public void Test1()
+    public void Assert_AboutUs_Page()
     {
+        var aboutUs = softUni_Driver.FindElement(By.CssSelector("li:nth-of-type(1) > .nav-link > .cell"));
 
+        var exepectedTitle = "За нас - Софтуерен университет";
+
+        aboutUs.Click();
+        Assert.That(softUni_Driver.Title, Is.EqualTo(exepectedTitle));
     }
 }
