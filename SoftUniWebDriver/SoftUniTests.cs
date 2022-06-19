fdaae51a633fc0c526b3928ce7a979f4011f1a7e
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2022-06-19 17:48:15 +0300

    add test for invalid user

--- SoftUniWebDriver/SoftUniTests.cs
+++ SoftUniWebDriver/SoftUniTests.cs
@@ -4,36 +4,52 @@ using OpenQA.Selenium.Chrome;
 
 public class SoftUniTests
 {
-    private ChromeDriver softUni_Driver;
+    private ChromeDriver driver;
 
 
     [OneTimeSetUp]
     public void SetUp()
     {
-        this.softUni_Driver = new ChromeDriver();
-        softUni_Driver.Url = "https://softuni.bg";
-        softUni_Driver.Manage().Window.Maximize();
+        this.driver = new ChromeDriver();
+        driver.Url = "https://softuni.bg";
+        driver.Manage().Window.Maximize();
     }
 
     [OneTimeTearDown]
-    public void Close() => softUni_Driver?.Close();
+    public void Close() => driver?.Close();
 
     [Test]
     public void Assert_Main_Page_Title()
     {
-        softUni_Driver.Url = "https://softuni.bg";
+        driver.Url = "https://softuni.bg";
         string exeptedTitle = "Обучение по програмиране - Софтуерен университет";
-        Assert.That(softUni_Driver.Title, Is.EqualTo(exeptedTitle));
+        Assert.That(driver.Title, Is.EqualTo(exeptedTitle));
     }
 
     [Test]
     public void Assert_AboutUs_Page()
     {
-        var aboutUs = softUni_Driver.FindElement(By.CssSelector("li:nth-of-type(1) > .nav-link > .cell"));
+        driver.Url = "https://softuni.bg";
+
+        var aboutUs = driver.FindElement(By.CssSelector("li:nth-of-type(1) > .nav-link > .cell"));
 
         var exepectedTitle = "За нас - Софтуерен университет";
 
         aboutUs.Click();
-        Assert.That(softUni_Driver.Title, Is.EqualTo(exepectedTitle));
+        Assert.That(driver.Title, Is.EqualTo(exepectedTitle));
+    }
+
+    [Test]
+    public void logOutLogIn()
+    {
+        driver.Navigate().GoToUrl("https://softuni.bg/");
+        driver.FindElement(By.CssSelector(".softuni-btn.softuni-btn-primary.softuni-btn-small")).Click();
+        driver.FindElement(By.CssSelector(".softuni-btn-primary")).Click();
+        driver.FindElement(By.Id("username")).SendKeys("User1");
+        driver.FindElement(By.Id("password-input")).SendKeys("password");
+        driver.FindElement(By.CssSelector(".softuni-btn")).Click();
+        Assert.That(driver.FindElement(By.CssSelector("li")).Text, Is.EqualTo("Невалидно потребителско име или парола"));
+        driver.Close();
+
     }
 }
